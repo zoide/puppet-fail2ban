b4bed5b605ce288e2020159f49f7d468077f02bc
Udo Waechter <root@zoide.net>
2011-12-17 23:13:26 +0100

    added fail2ban::jail

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 class fail2ban ($jails = [],
     $mailto = "",
     $custom_jails = "",
-    $ignoreip="127.0.0.1") {
+    $ignoreip = "127.0.0.1") {
     package {
         ["fail2ban", "gamin", "iptables"] :
             ensure => installed ;
@@ -15,26 +15,33 @@ class fail2ban ($jails = [],
             ensure => running,
             enable => true ;
     }
-    file {
-        "/etc/fail2ban/filter.d/mail.conf" :
-            mode => 644,
-            owner => root,
-            group => root,
-            source => "puppet:///modules/fail2ban/mail.conf",
-            require => Package["fail2ban"],
-            notify => Service["fail2ban"] ;
-
-        "/etc/fail2ban/jail.local" :
-            mode => 644,
-            owner => root,
-            group => root,
-            content => template("fail2ban/jail.local.erb"),
-            require => Package["fail2ban"],
-            notify => Service["fail2ban"] ;
+    $f2b_etc = "/etc/fail2ban"
+    $jail_local_d = "${f2b_etc}/jail.local.d"
+    File {
+        owner => root,
+        group => root,
+        require => Package["fail2ban"],
+        notify => Exec["fail2ban.local-generate"],
     }
+    file {
+        "${f2b_etc}/filter.d/mail.conf" :
+            mode => 644,
+            source => "puppet:///modules/fail2ban/mail.conf" ;
 
+        "${jail_local_d}/00_jail.local" :
+            mode => 644,
+            content => template("fail2ban/jail.local.erb") ;
+
+        "${jail_local_d}" :
+            ensure => "directory",
+    }
+    exec {
+        "fail2ban.local-generate" :
+            command => "cat ${jail_local_d}/* >${f2b_etc}/jail.local",
+            refreshonly => true,
+            notify => Service["fail2ban"]
+    }
     # munin
-
     # TODO better way to know if node is including munin class
     if $munin_graphs {
         file {
@@ -45,10 +52,33 @@ class fail2ban ($jails = [],
                 notify => Service["munin-node"] ;
 
             "/etc/munin/plugin-conf.d/all_jails" :
-                content => "[all_jails]
-user root",
+                content => "[all_jails]\nuser root",
                 require => Package[$munin],
                 notify => Service["munin-node"] ;
+        }
+    }
+    define jail ($ensure = "present",
+        $jail_name = "",
+        $enabled = true,
+        $filter = "",
+        $action,
+        $mailto = "",
+        logpath,
+        maxretry = 5,
+        order = 99) {
+        $jail_name_r = $jail_name ? {
+            "" => $name,
+            default => $jail_name,
+        }
+        $filter_r = $filter ? {
+            "" => $jail_name_r,
+            default => $filter,
+        }
+        file {
+            "${fail2ban::jail_local_d}/${order}-${jail_name_r}" :
+                ensure => $ensure,
+                content => template("fail2ban/jail.local.snipp.erb"),
+                notify => Exec["fail2ban.local-generate"],
         }
     }
 }
